5d83e070ae8352d51e0f96e03b5a2ae7ecfda0ee
Malkit Singh <rgcsm.pb@gmail.com>
2019-12-12 22:37:57 +0530

    resource pass to api data

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,7 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    public function user()
+    public function getRouteKeyName(){
+        return 'slug';
+    }
+//   protected $fillable = ['title','slug','body','category_id','user_id'] ;
+    protected $guarded =[];
+  public function user()
     {
         return $this->belongsTo(User::class);
     }
@@ -20,5 +25,10 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
